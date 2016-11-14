38d73e23da83a07bb2953471257d7b74ee6be245
imac <imac@iMacdAlexandre3.(none)>
2016-11-14 11:33:33 +0100

    UI

--- batonnets/ViewController.swift
+++ batonnets/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,39 +19,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    
-    
-    
-    
-    
-/*
-    var nbAllumettesRestantes:Int = 20
-    var joueurActuel:Int = 1
-    while nbAllumettesRestantes > 0 {
-        print("Il reste \(nbAllumettesRestantes) allumette(s)")
-        var saisieJoueur:Int
-        let saisieMax = min(3, nbAllumettesRestantes)
-        if joueurActuel == 2 {
-            saisieJoueur = saisieIA(nbAllumettesDisponibles: nbAllumettesRestantes)
-        } else {
-            repeat {
-                print("Joueur \(joueurActuel), combien d'allumettes ?")
-                saisieJoueur = readInt()
-            } while saisieJoueur < 1 || saisieJoueur > saisieMax
-        }
-        nbAllumettesRestantes = nbAllumettesRestantes - saisieJoueur
-    
-        if joueurActuel == 1 {
-            joueurActuel = 2
-        } else {
-            joueurActuel = 1
-        }
-    }
-    print("Le joueur \(joueurActuel) a gagné")
-*/
-    
-    
+     
     
 }
 
---
+++ batonnets/gamer.swift
@@ -0,0 +1,19 @@
+//
+//  gamer.swift
+//  batonnets
+//
+//  Created by imac on 13/11/2016.
+//  Copyright © 2016 imac. All rights reserved.
+//
+
+import Foundation
+
+class Gamer {
+    var name:String
+    var score:Int
+    
+    init(name:String, score:Int) {
+        self.name = name
+        self.score = score
+    }
+}
